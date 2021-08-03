4e1a2a00159899979994e3d1cc2bb0789ff46341
Simone Ballard <simone.a.ballard@gmail.com>
2021-08-03 12:02:15 -0600

    group text coming through need to test with a picture

--- src/components/howitworks/HSection1.vue
+++ src/components/howitworks/HSection1.vue
@@ -4,6 +4,7 @@
     <img class="item__img" :src="item.img.url" :alt="item.img.alt">
     <div class="item__title">{{ item.title[0].text }}</div>
     <div class="item__text">{{ item.text[0].text }}</div>
+    <div class="item__text">{{ item.text[1].text }}</div>
   </div>
 </div>
 </template>
