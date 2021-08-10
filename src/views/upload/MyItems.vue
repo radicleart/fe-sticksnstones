1dd8436bbfcd1305574a4c1f6fd737d660467a09
Simone Ballard <simone.a.ballard@gmail.com>
2021-08-10 17:53:52 -0600

    nav of my items updated

--- src/views/upload/MyItems.vue
+++ src/views/upload/MyItems.vue
@@ -2,20 +2,22 @@
 <div class="container" v-if="loaded">
   <div class="mb-5" :key="componentKey">
     <h1>Your NFTs</h1>
-    <div class="d-flex justify-content-end">
-      <b-button class="ml-3" :variant="(filter === 'all') ? 'info' : 'light'" @click="updateFilter('all')">All</b-button>
-      <b-button class="ml-3" :variant="(filter === 'uploaded') ? 'info' : 'light'" @click="updateFilter('uploaded')">Uploaded</b-button>
-      <b-button class="ml-3" :variant="(filter === 'minted') ? 'info' : 'light'" @click="updateFilter('minted')">Minted</b-button>
-      <b-button class="ml-3" :variant="(filter === 'onsale') ? 'info' : 'light'" @click="updateFilter('onsale')">On Sale</b-button>
-      <b-button class="ml-3" :variant="(filter === 'sold') ? 'info' : 'light'" @click="updateFilter('sold')">Sold</b-button>
-    </div>
+    <b-nav class="d-flex justify-content-center">
+      <b-nav-item class="ml-3" :variant="(filter === 'all') ? 'info' : 'light'" @click="updateFilter('all')">All</b-nav-item>
+      <b-nav-item class="ml-3" :variant="(filter === 'uploaded') ? 'info' : 'light'" @click="updateFilter('uploaded')">Uploaded</b-nav-item>
+      <b-nav-item class="ml-3" :variant="(filter === 'minted') ? 'info' : 'light'" @click="updateFilter('minted')">Minted</b-nav-item>
+      <b-nav-item class="ml-3" :variant="(filter === 'onsale') ? 'info' : 'light'" @click="updateFilter('onsale')">On Sale</b-nav-item>
+      <b-nav-item class="ml-3" :variant="(filter === 'sold') ? 'info' : 'light'" @click="updateFilter('sold')">Sold</b-nav-item>
+    </b-nav>
     <b-row :key="componentKey" class="mb-4" v-if="filteredItems && filteredItems.length > 0">
       <b-col md="4" sm="4" xs="6" v-for="(item, index) in filteredItems" :key="index" class="mt-5">
         <SingleItem class="mb-2" :item="item"/>
       </b-col>
     </b-row>
   </div>
+  <div class="d-flex justify-content-center">
   <b-button variant="warning" class="mr-2 mb-2"><b-link class="text-white" to="/create">Upload New Item</b-link></b-button>
+  </div>
 </div>
 <div v-else class="center text-center">
   <LoopbombSpinner />
