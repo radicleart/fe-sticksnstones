e478a9d76e651dc2cbc391dab57e03c896c9c5fe
Simone Ballard <simone.a.ballard@gmail.com>
2021-09-08 12:09:37 -0600

    removed FAQ from router

--- src/router/index.ts
+++ src/router/index.ts
@@ -17,7 +17,6 @@ import Model from '../views/Models/Model.vue'
 import About from '../views/About.vue'
 import Terms from '../views/Terms.vue'
 import Privacy from '../views/Privacy.vue'
-import FAQ from '../views/FAQ.vue'
 import AssetDetails from '../views/AssetDetails.vue'
 import ItemPreview from '../views/ItemPreview.vue'
 import MyNfts from '../views/upload/MyNfts.vue'
@@ -127,11 +126,6 @@ const routes: Array<RouteConfig> = [
     path: '/about',
     name: 'about',
     components: { default: About, header: MainNavbar, footer: MainFooter }
-  },
-  {
-    path: '/faq',
-    name: 'faq',
-    components: { default: FAQ, header: MainNavbar, footer: MainFooter }
   },
   {
     path: '/terms',
